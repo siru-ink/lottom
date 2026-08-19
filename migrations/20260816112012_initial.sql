16b7530c4b541b84013f4684ffa2671d19cd8cf9
siru-ink <mail@siru.ink>
2026-08-19 14:16:54 +0200

    Draft for database design

---
+++ migrations/20260816112012_initial.sql
@@ -0,0 +1,47 @@
+-- Create relations needed for authentication and session management
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS sessions (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) NOT NULL,
+    start TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+-- Create relations needed for shopping lists management
+CREATE TABLE IF NOT EXISTS lists (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS items (
+    id SERIAL PRIMARY KEY,
+    list_id INTEGER REFERENCES lists(id),
+    en_name TEXT NOT NULL,
+    zh_name TEXT,
+    de_name TEXT,
+    img_path TEXT,
+    estimated_euro_price INTEGER
+);
+CREATE TABLE IF NOT EXISTS lists_users_mapping (
+    list_id INTEGER REFERENCES lists(id) NOT NULL,
+    user_id INTEGER REFERENCES users(id) NOT NULL,
+    role_id INTEGER REFERENCES roles(id) NOT NULL
+);
+
+-- Create relations needed for storing known shopping list items
+CREATE TABLE IF NOT EXISTS prefill_item (
+    id SERIAL PRIMARY KEY,
+    en_name TEXT UNIQUE NOT NULL,
+    zh_name TEXT UNIQUE NOT NULL,
+    de_name TEXT UNIQUE NOT NULL,
+    euro_price INTEGER NOT NULL
+);
+
+-- Create default user list access roles
+INSERT INTO roles (name) VALUES ('owner'), ('editor'), ('viewer');

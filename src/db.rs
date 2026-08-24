e13b2ca1874f8c4d0f8fa0bc5a06d770c2850d55
siru-ink <mail@siru.ink>
2026-08-24 17:11:01 +0200

    Added read fn for User db relation

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,24 @@
 use sqlx::migrate::{MigrateError, Migrator};
 use sqlx::postgres::PgPool;
+use sqlx::query_as;
 use std::path::Path;
 
 pub async fn apply_db_migrations(pool: &PgPool) -> Result<(), MigrateError> {
     let migrator = Migrator::new(Path::new("./migrations")).await?;
     migrator.run(pool).await
+}
+
+#[derive(Debug)]
+struct User {
+    id: i32,
+    name: String,
+    password: String,
+    default_list_id: i32,
+}
+
+async fn read_user(pool: &PgPool, user_id: i32) -> Option<User> {
+    query_as!(User, "SELECT * FROM users WHERE id = $1", user_id)
+        .fetch_optional(pool)
+        .await
+        .ok()?
 }

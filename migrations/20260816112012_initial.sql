90bcf810935d8dd46865ae8e962613459c891b0c
siru-ink <mail@siru.ink>
2026-08-24 17:10:22 +0200

    Updated initial database schema

--- migrations/20260816112012_initial.sql
+++ migrations/20260816112012_initial.sql
@@ -1,21 +1,5 @@
--- Create relations needed for authentication and session management
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    password TEXT NOT NULL
-);
-CREATE TABLE IF NOT EXISTS sessions (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) NOT NULL,
-    start TIMESTAMP WITH TIME ZONE NOT NULL
-);
-
 -- Create relations needed for shopping lists management
 CREATE TABLE IF NOT EXISTS lists (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
-);
-CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL
 );
@@ -27,6 +11,25 @@ CREATE TABLE IF NOT EXISTS items (
     de_name TEXT,
     img_path TEXT,
     estimated_euro_price INTEGER
+);
+
+-- Create relations needed for authentication and session management
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    default_list_id INTEGER REFERENCES lists(id) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS sessions (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) NOT NULL,
+    start TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+-- Create relations needed for shared lists access
+CREATE TABLE IF NOT EXISTS roles (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 CREATE TABLE IF NOT EXISTS lists_users_mapping (
     list_id INTEGER REFERENCES lists(id) NOT NULL,

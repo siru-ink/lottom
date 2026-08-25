62ab0a41d394c0afe873157764c3af8d432eaf97
siru-ink <mail@siru.ink>
2026-08-25 18:48:23 +0200

    Added create fn for User db relation

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,6 @@
 use sqlx::migrate::{MigrateError, Migrator};
 use sqlx::postgres::PgPool;
-use sqlx::query_as;
+use sqlx::{query, query_as};
 use std::path::Path;
 
 pub async fn apply_db_migrations(pool: &PgPool) -> Result<(), MigrateError> {
@@ -14,6 +14,27 @@ struct User {
     name: String,
     password: String,
     default_list_id: i32,
+}
+
+async fn create_user(pool: &PgPool, new_name: String, new_password: String) -> Option<i32> {
+    let new_list_id = query!("INSERT INTO lists (name) VALUES ('default') RETURNING id")
+        .fetch_one(pool)
+        .await
+        .ok()?
+        .id;
+
+    let new_user_id = query!(
+        "INSERT INTO users (name, password, default_list_id) VALUES ($1, $2, $3) RETURNING id",
+        new_name,
+        new_password,
+        new_list_id
+    )
+    .fetch_one(pool)
+    .await
+    .ok()?
+    .id;
+
+    Some(new_user_id)
 }
 
 async fn read_user(pool: &PgPool, user_id: i32) -> Option<User> {

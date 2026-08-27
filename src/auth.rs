ecf6aee2a4ab7701b005bad792fc95ed32d452ce
siru-ink <mail@siru.ink>
2026-08-27 11:54:47 +0200

    DRAFT: Added function on User for checking password

--- src/auth.rs
+++ src/auth.rs
@@ -1,11 +1,19 @@
-use crate::AppState;
-use axum::Router;
+use crate::db::PasswordCheckResult;
+use crate::{AppState, db};
 use axum::extract::State;
 use axum::http::StatusCode;
-use axum::response::{Html, IntoResponse};
+use axum::response::{Html, IntoResponse, Redirect};
 use axum::routing::get;
+use axum::{Form, Router};
+use serde::Deserialize;
 use std::sync::Arc;
 use tera::context;
+
+#[derive(Deserialize)]
+struct LoginForm {
+    user_id: String,
+    password: String,
+}
 
 async fn get_login(State(appstate): State<Arc<AppState>>) -> impl IntoResponse {
     let context = context! {};
@@ -23,8 +31,24 @@ async fn get_login(State(appstate): State<Arc<AppState>>) -> impl IntoResponse {
 }
 
 #[axum::debug_handler]
-async fn post_login() -> &'static str {
-    "This is the post login page."
+async fn post_login(
+    State(appstate): State<Arc<AppState>>,
+    Form(login_form): Form<LoginForm>,
+) -> impl IntoResponse {
+    let user_id: i32 = match login_form.user_id.parse() {
+        Ok(number) => number,
+        Err(_) => return Redirect::to("/auth/login"),
+    };
+
+    let user = match db::read_user(&appstate._pg_pool, user_id).await {
+        Some(user) => user,
+        None => return Redirect::to("/auth/login"),
+    };
+
+    match user.check_password(&login_form.password) {
+        PasswordCheckResult::Valid => Redirect::to("/"),
+        PasswordCheckResult::Invalid => Redirect::to("/auth/login"),
+    }
 }
 
 async fn get_logout() -> &'static str {
--- src/db.rs
+++ src/db.rs
@@ -9,11 +9,26 @@ pub async fn apply_db_migrations(pool: &PgPool) -> Result<(), MigrateError> {
 }
 
 #[derive(Debug)]
-struct User {
+pub struct User {
     id: i32,
     name: String,
     password: String,
     default_list_id: i32,
+}
+
+pub enum PasswordCheckResult {
+    Valid,
+    Invalid,
+}
+
+impl User {
+    pub fn check_password(&self, comparison_password: &String) -> PasswordCheckResult {
+        if &self.password == comparison_password {
+            PasswordCheckResult::Valid
+        } else {
+            PasswordCheckResult::Invalid
+        }
+    }
 }
 
 async fn create_user(pool: &PgPool, new_name: &str, new_password: &str) -> Option<i32> {
@@ -36,7 +51,7 @@ async fn create_user(pool: &PgPool, new_name: &str, new_password: &str) -> Optio
     Some(new_user_id)
 }
 
-async fn read_user(pool: &PgPool, user_id: i32) -> Option<User> {
+pub async fn read_user(pool: &PgPool, user_id: i32) -> Option<User> {
     query_as!(User, "SELECT * FROM users WHERE id = $1", user_id)
         .fetch_optional(pool)
         .await

2f86aed25df2c064cda1d45a6b79795416c04ccc
siru-ink <mail@siru.ink>
2026-09-02 14:26:27 +0200

    Draft state (working on log in with redirect)

--- src/routing/auth.rs
+++ src/routing/auth.rs
@@ -1,15 +1,22 @@
 use crate::AppState;
+use crate::cookies::set_cookie;
+use crate::db::{
+    session::Session,
+    user::{CheckPasswordResult, User},
+};
 use crate::extractor::auth::AuthenticatedUser;
 use crate::extractor::flash::Flash;
 use crate::routing::html_error;
 use axum::extract::Query;
 use axum::{
-    extract::State,
+    extract::{Form, State},
     response::{Html, IntoResponse, Redirect, Response},
 };
+use serde::Deserialize;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tera::Context;
+use tower_cookies::Cookies;
 
 pub async fn get_login(
     State(appstate): State<Arc<AppState>>,
@@ -47,42 +54,69 @@ pub async fn test_auth(_user: AuthenticatedUser) -> Response {
     "You are now logged in.".into_response()
 }
 
-// #[derive(Deserialize)]
-// struct LoginForm {
-//     user_id: String,
-//     password: String,
-// }
+#[derive(Deserialize)]
+struct LoginForm {
+    user_id: String,
+    password: String,
+    forward_to: String,
+}
 
-// async fn post_login(
-//     State(appstate): State<Arc<AppState>>,
-//     cookies: Cookies,
-//     Form(login_form): Form<LoginForm>,
-// ) -> Response {
-//     // Check whether the provided user id is in fact a number
-//     let user_id: i32 = match login_form.user_id.parse() {
-//         Ok(number) => number,
-//         Err(_) => return Redirect::to("/auth/login").into_response(),
-//     };
+async fn post_login(
+    State(appstate): State<Arc<AppState>>,
+    cookies: Cookies,
+    Form(login_form): Form<LoginForm>,
+    flash: Flash,
+) -> Response {
+    let redirect_uri = urlencoding::encode(&login_form.forward_to).to_owned();
 
-//     let user = match db::user::User::read(&appstate.pg_pool, user_id).await {
-//         Some(user) => user,
-//         None => return Redirect::to("/auth/login"),
-//     };
+    // Check whether the provided user id is in fact a number
+    let user_id: i32 = match login_form.user_id.parse() {
+        Ok(number) => number,
+        Err(_) => {
+            flash.set("Please enter a numerical user id to log in.");
+            return Redirect::to(&format!("/auth/login?forward_to={}", redirect_uri))
+                .into_response();
+        }
+    };
 
-//     let _ = match user.check_password(&login_form.password) {
-//         CheckPasswordResult::PasswordCorrect => (),
-//         CheckPasswordResult::PasswordWrong => return Redirect::to("/auth/login"),
-//     };
+    let user = match User::read(&appstate.pg_pool, user_id).await {
+        Some(user) => user,
+        None => {
+            flash.set("The provided user id does not exist.");
+            return Redirect::to(&format!("/auth/login?forward_to={}", redirect_uri))
+                .into_response();
+        }
+    };
 
-//     // Create a new session
-//     let new_session = match db::session::Session::create(&appstate.pg_pool, &user).await {
-//         Some(new_session) => new_session,
-//         None => return Redirect::to("/auth/login"),
-//     };
+    let _ = match user.check_password(&login_form.password) {
+        CheckPasswordResult::PasswordCorrect => (),
+        CheckPasswordResult::PasswordWrong => {
+            flash.set("The provided password was not correct.");
+            return Redirect::to(&format!("/auth/login?forward_to={}", redirect_uri))
+                .into_response();
+        }
+    };
 
-//     // Store the session id as a private cookie
-//     match set_cookie(new_session.as_cookie_value(), cookies) {
-//         Ok(_) => return Redirect::to("/"),
-//         Err(e) => return Redirect::to("/auth/login"),
-//     }
-// }
+    // Create a new session
+    let new_session = match Session::create(&appstate.pg_pool, &user).await {
+        Some(new_session) => new_session,
+        None => {
+            flash.set(
+                "Logging in failed because no session id could be created. \
+                Please try again or contact the system administrator.",
+            );
+            return Redirect::to(&format!("/auth/login?forward_to={}", redirect_uri))
+                .into_response();
+        }
+    };
+
+    // Store the session id as a private cookie
+    match set_cookie(new_session.as_cookie_value(), cookies) {
+        Ok(_) => return Redirect::to(&redirect_uri).into_response(),
+        Err(_) => {
+            flash.set("Please allow cookies for this domain/website to be able to log in.");
+            return Redirect::to(&format!("/auth/login?forward_to={}", redirect_uri))
+                .into_response();
+        }
+    }
+}

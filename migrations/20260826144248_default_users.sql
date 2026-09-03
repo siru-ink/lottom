db18901719e2ebaa9c3a05fa41c22095771beeb9
siru-ink <mail@siru.ink>
2026-09-03 13:42:05 +0200

    Hardcode the admin id for now

--- migrations/20260826144248_default_users.sql
+++ migrations/20260826144248_default_users.sql
@@ -1,3 +1,3 @@
 -- Add default admin user
 INSERT INTO lists (id, name) VALUES (1, 'admin_default');
-INSERT INTO users (name, password, default_list_id) VALUES ('admin', 'test', 1);
+INSERT INTO users (id, name, password, default_list_id) VALUES (1, 'admin', 'test', 1);

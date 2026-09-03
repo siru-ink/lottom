d241af180f8efe5c5473f62d5f561300bd28c51d
siru-ink <mail@siru.ink>
2026-09-03 14:36:41 +0200

    Refactored route dispatch
    Nesting routes prevents extractors from being able to access full uri.
    They only get the last unmatched part.

--- src/routing/mod.rs
+++ src/routing/mod.rs
@@ -6,13 +6,8 @@ mod auth;
 mod list;
 
 pub fn get_routes() -> Router<Arc<AppState>> {
-    let auth_router = Router::new()
-        .route("/login", get(auth::get_login).post(auth::post_login))
-        .route("/logout", get(auth::get_logout));
-
-    let list_router = Router::new().route("/", get(list::get_list));
-
     Router::new()
-        .nest("/auth", auth_router)
-        .nest("/list", list_router)
+        .route("/auth/login", get(auth::get_login).post(auth::post_login))
+        .route("/auth/logout", get(auth::get_logout))
+        .route("/list", get(list::get_list))
 }

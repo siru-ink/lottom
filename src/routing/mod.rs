4ee49dc8d38f501e53c14ae132f97209953f2156
siru-ink <mail@siru.ink>
2026-09-03 12:38:48 +0200

    Removed old 404 page not found handler

--- src/routing/mod.rs
+++ src/routing/mod.rs
@@ -3,10 +3,11 @@ use axum::{Router, routing::get};
 use std::sync::Arc;
 
 mod auth;
-mod html_error;
 
 pub fn get_routes() -> Router<Arc<AppState>> {
-    let auth_router = Router::new().route("/login", get(auth::get_login).post(auth::post_login));
+    let auth_router = Router::new()
+        .route("/login", get(auth::get_login).post(auth::post_login))
+        .route("/logout", get(auth::get_logout));
 
     Router::new().nest("/auth", auth_router)
 }

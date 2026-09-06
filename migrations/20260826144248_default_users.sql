18d648c7e96d1108139eaa12fcdebefc44f59bd9
siru-ink <mail@siru.ink>
2026-09-06 12:26:03 +0200

    Remove unneeded hard coded admin user setup

--- migrations/20260826144248_default_users.sql
+++
@@ -1,4 +0,0 @@
--- Add default admin user
-INSERT INTO lists (id, name) VALUES (1, 'admin_default');
-INSERT INTO users (id, name, password, default_list_id) VALUES (1, 'admin', 'test', 1);
-INSERT INTO lists_users_mapping (user_id, list_id, role_id) VALUES (1, 1, 1);

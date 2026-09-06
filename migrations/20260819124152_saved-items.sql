039e5c76445f8d66a9a397798b30d087bd686149
siru-ink <mail@siru.ink>
2026-09-06 12:33:37 +0200

    Update prefill items list

--- migrations/20260819124152_saved-items.sql
+++ migrations/20260819124152_saved-items.sql
@@ -1,5 +1,6 @@
 -- Add default items to the saved items table
-INSERT INTO prefill_items (en_name, de_name, zh_name, euro_price)
+INSERT INTO prefill_items (en_name, zh_name, de_name, euro_price)
 VALUES
-    ('garlic', 'Knoblauch', '', 100)
+    ('Cow milk', '牛奶', 'Kuhmilch', 95),
+    ('Chicken mortadella', '鸡肉莫塔德拉香肠', 'Geflügel-Mortadella', 129)
 ;

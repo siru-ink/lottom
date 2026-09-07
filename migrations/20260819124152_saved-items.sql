231d2bdcecbb1f3ff37816c9f1cf13b83d36a27a
siru-ink <mail@siru.ink>
2026-09-07 12:07:14 +0200

    Update prefill items list

--- migrations/20260819124152_saved-items.sql
+++ migrations/20260819124152_saved-items.sql
@@ -2,5 +2,13 @@
 INSERT INTO prefill_items (en_name, zh_name, de_name, euro_price)
 VALUES
     ('Cow milk', '牛奶', 'Kuhmilch', 95),
-    ('Chicken mortadella', '鸡肉莫塔德拉香肠', 'Geflügel-Mortadella', 129)
+    ('Chicken mortadella', '鸡肉莫塔德拉香肠', 'Geflügel-Mortadella', 129),
+    ('Banana', '香蕉', 'Banane', 249),
+    ('Thin beef steaks', '薄切牛排', 'Rinder-Minutensteaks', 599),
+    ('Bell peppers', '甜椒', 'Paprika', 299),
+    ('Chanterelle mushrooms', '鸡油菌', 'Pfifferlinge', 479),
+    ('Tortilla chips', '玉米片', 'Totilla Chips', 99),
+    ('Soy-Yoghurt Alpro Strawberry', 'Alpro草莓味豆酸奶', 'Soja Joghurt Alpro Erdbeere', 219),
+    ('Soy-Yoghurt Alpro Natural', 'Alpro原味豆酸奶', 'Soja Joghurt Alpro Natur', 219),
+    ('Soy-Yoghurt Alpro Peach', 'Alpro桃味豆酸奶', 'Soja Joghurt Alpro Pfirsich', 219),
 ;
